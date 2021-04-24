2950f00cd077cf581249b42085b181a8fd7b0b86
gav427 <102111726@student.swin.edu.au>
2021-04-25 02:28:11 +1000

    InitializeCameras.cs: now finds 'Player' object only if it has CharacterController component + private method renamed (FindHighestLevelPlayerObject() -> FindPlayerObject()

--- Assets/Scripts/InitializeCameras.cs
+++ Assets/Scripts/InitializeCameras.cs
@@ -22,7 +22,7 @@ public class InitializeCameras : MonoBehaviour
             {
                 try
                 {
-                    playerObject = FindHighestLevelPlayerObject(ref gameObjects);
+                    playerObject = FindPlayerObject(ref gameObjects);
                 } catch (Exception e)
                 {
                     Debug.Log(e);
@@ -41,22 +41,31 @@ public class InitializeCameras : MonoBehaviour
         }
     }
 
-    private GameObject FindHighestLevelPlayerObject(ref GameObject[] list, GameObject gObj = null)
+    private GameObject FindPlayerObject(ref GameObject[] gArr, GameObject gObj = null)
     {
         if (gObj)
         {
-            GameObject gObjParent = gObj.transform.parent.gameObject;
+            GameObject gObjParent;
 
-            if (gObjParent.CompareTag("Player")) return FindHighestLevelPlayerObject(ref list, gObjParent);
-            else return gObj;
+            if (gObj.transform != gObj.transform.root) gObjParent = gObj.transform.parent.gameObject;
+            else gObjParent = null; //throw new Exception("'Player' GameObject not found!");
+
+            if (gObj.GetComponent<CharacterController>() || !gObjParent) return gObj; //BUG: leaving here when child has CharacterController
+            else if (gObjParent && gObjParent.CompareTag("Player")) return FindPlayerObject(ref gArr, gObjParent);
+            else throw new Exception("'Player' GameObject not found!"); //return gObj;
         } else
         {
-            foreach (GameObject g in list)
+            //foreach (GameObject g in list)
+            for (int i = gArr.Length - 1; i >= 0; i--)
             {
-                GameObject gParent = g.transform.parent.gameObject;
+                GameObject gParent;
 
-                if (gParent.CompareTag("Player")) return FindHighestLevelPlayerObject(ref list, gParent);
-                else return g;
+                if (gArr[i].transform != gArr[i].transform.root) gParent = gArr[i].transform.parent.gameObject;
+                else gParent = null; //throw new Exception("'Player' GameObject not found!");
+
+                if (gArr[i].GetComponent<CharacterController>() || !gParent) return gArr[i]; //BUG: leaving here when child has CharacterController
+                else if (gParent && gParent.CompareTag("Player")) return FindPlayerObject(ref gArr, gParent);
+                else throw new Exception("'Player' GameObject not found!"); //return g;
             }
 
             throw new Exception("List of 'Players' is null; are you sure there is a GameObject tagged 'Player'?");
