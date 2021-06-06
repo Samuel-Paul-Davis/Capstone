25ba290a38d4554c678f96532320d12947b7fad5
gav427 <102111726@student.swin.edu.au>
2021-06-06 17:34:31 +1000

    BUGFIX: game no longer remains paused after pause menu is exited

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -12,7 +12,7 @@ public class CanvasController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        GamePausePanel.SetActive(false);
+        Resume();
     }
 
     // Update is called once per frame
