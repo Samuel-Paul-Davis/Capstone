ddeb5dfdfc40423efc293d375646a2d976967712
Samuel-Paul-Davis <101093250@student.swin.edu.au>
2021-08-07 13:03:16 +1000

    Added the discover mechanic to be used in tamdem with the scanner

--- Assets/Scripts/AbstractObjectInteraction.cs
+++ Assets/Scripts/AbstractObjectInteraction.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public abstract class AbstractObjectInteraction : MonoBehaviour
 {
+    protected bool isDiscovered = false; // Used for the scanning tool, call the function, "Discover" to make True
     public abstract void ObjectInteraction(); // The function that is called when the object is selected
     public abstract void ObjectDeselectInteraction(); // The function that is called when the object is deselected, this can be left blank if no additional interaction is required
+
+    // This can be overridden by children so that they can do specific things when they are discovered, such as change materials or make things appear
+    public virtual void Discover()
+    {
+        isDiscovered = true;
+    }
 }
