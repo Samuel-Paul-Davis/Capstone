91945244deaab944b72b090aac10edea40452fb9
gav427 <102111726@student.swin.edu.au>
2021-08-14 21:43:21 +1000

    adding WIP reader slot system

--- Assets/Scripts/CardReader.cs
+++ Assets/Scripts/CardReader.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class CardReader : MonoBehaviour
 {
+    public GameObject slot1_transform;
+    public GameObject slot2_transform;
+
+    private GameObject slot1_core;
+    private GameObject slot2_core;
+
+
+    /* Bugs:
+     *      - As OnTriggerEnter runs on each FixedUpdate, the other object is being added twice
+     */
     private void OnTriggerEnter(Collider other)
     {
+        if (slot1_core != null && slot2_core != null)
+            return;
+
         if (other.GetComponent<KeyCoreObject>()) {
             other.GetComponent<KeyCoreObject>().isMoving = false;
             other.GetComponent<KeyCoreObject>().isActive = false;
 
-            other.transform.position = transform.position;
+            if (slot1_core == null)
+            {
+                other.transform.position = slot1_transform.transform.position;
+                slot1_core = other.gameObject;
+            }
+            else
+            {
+                other.transform.position = slot2_transform.transform.position;
+                slot2_core = other.gameObject;
+            }
 
             other.GetComponent<Rigidbody>().isKinematic = true;
         }
