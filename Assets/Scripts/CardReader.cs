43bf539a1e995ffc3310f04ef00bab981a249a79
gav427 <102111726@student.swin.edu.au>
2021-08-15 11:51:53 +1000

    CardReader.cs: broken error checking (adding for reference later)

--- Assets/Scripts/CardReader.cs
+++ Assets/Scripts/CardReader.cs
@@ -4,35 +4,52 @@ using UnityEngine;
 
 public class CardReader : MonoBehaviour
 {
-    public GameObject slot1_transform;
-    public GameObject slot2_transform;
+    public GameObject slot1;
+    public GameObject slot2;
 
-    private GameObject slot1_core;
-    private GameObject slot2_core;
-
+    private List<GameObject> cores = new List<GameObject>() { null, null };
 
     /* Bugs:
      *      - As OnTriggerEnter runs on each FixedUpdate, the other object is being added twice
      */
     private void OnTriggerEnter(Collider other)
     {
-        if (slot1_core != null && slot2_core != null)
+        if (cores[0] != null && cores[1] != null)
             return;
 
-        if (other.GetComponent<KeyCoreObject>()) {
+        if (other.GetComponent<KeyCoreObject>())
+        {
+            int i = 0;
+            /*while (i < cores.Count) //infinite loop with (Fixed)Update()
+            {
+                Debug.Log("Line 25");
+                if (cores[0] == null && cores[1] == null)
+                    break;
+
+                Debug.Log("Line 28");
+
+                if (cores[i].GetComponent<KeyCoreObject>().id == other.GetComponent<KeyCoreObject>().id) {
+                    return;
+                }
+            }*/
+
+            Debug.Log("Line 33");
+        
             other.GetComponent<KeyCoreObject>().isMoving = false;
             other.GetComponent<KeyCoreObject>().isActive = false;
 
-            if (slot1_core == null)
+            if (cores[0] == null)
             {
-                other.transform.position = slot1_transform.transform.position;
-                slot1_core = other.gameObject;
+                other.transform.position = slot1.transform.position;
+                cores[0] = other.gameObject;
             }
             else
             {
-                other.transform.position = slot2_transform.transform.position;
-                slot2_core = other.gameObject;
+                other.transform.position = slot2.transform.position;
+                cores[1] = other.gameObject;
             }
+
+            Debug.Log("Line 49");
 
             other.GetComponent<Rigidbody>().isKinematic = true;
         }
