8df9504b7a49b9f31ff64c1cc9aa4746060abc91
gav427 <102111726@student.swin.edu.au>
2021-08-20 17:52:31 +1000

    KeyReader.cs: adding the ability to preset a key in position

--- Assets/Scripts/KeyReader.cs
+++ Assets/Scripts/KeyReader.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class KeyReader : MonoBehaviour
 {
+    public bool unlocked = false;
+
     public GameObject slot1;
     public GameObject slot2;
 
-    public bool unlocked = false;
-
+    [SerializeField]
     private GameObject slot1_key;
+    [SerializeField]
     private GameObject slot2_key;
+
+    private void Start()
+    {
+        //if DRY could cry, oh my, why?
+        if (slot1_key)
+        {
+            slot1_key.GetComponent<KeyCoreObject>().isActive = false;
+            slot1_key.GetComponent<KeyCoreObject>().isMoving = false;
+            slot1_key.GetComponent<Rigidbody>().isKinematic = true;
+
+            slot1_key.transform.position = slot1.transform.position;
+            slot1.GetComponent<Collider>().enabled = false;
+
+            slot1_key.GetComponent<KeyCoreObject>().enabled = false;
+            slot1_key.tag = "Untagged";
+        }
+
+        if (slot2_key)
+        {
+            slot2_key.GetComponent<KeyCoreObject>().isActive = false;
+            slot2_key.GetComponent<KeyCoreObject>().isMoving = false;
+            slot2_key.GetComponent<Rigidbody>().isKinematic = true;
+
+            slot2_key.transform.position = slot2.transform.position;
+            slot2.GetComponent<Collider>().enabled = false;
+
+            slot2_key.GetComponent<KeyCoreObject>().enabled = false;
+            slot2_key.tag = "Untagged";
+        }
+    }
 
     private void Update()
     {
