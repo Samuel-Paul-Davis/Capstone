2db2a042365a4fc363ea81f8eba90f18b1ca7d00
gav427 <102111726@student.swin.edu.au>
2021-08-21 18:32:17 +1000

    KeyReader.cs: code cleanup & comment

--- Assets/Scripts/KeyReader.cs
+++ Assets/Scripts/KeyReader.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Missing Feature: Should accept slots being prefilled in the Unity Inspector
+/// </summary>
 public class KeyReader : SlotPuzzle
 {
     public bool unlocked = false;
@@ -19,34 +23,4 @@ public class KeyReader : SlotPuzzle
             }
         }
     }
-
-    /*private void Start()
-    {
-        //if DRY could cry, oh my, why?
-        if (slot1_key)
-        {
-            slot1_key.GetComponent<KeyCoreObject>().isActive = false;
-            slot1_key.GetComponent<KeyCoreObject>().isMoving = false;
-            slot1_key.GetComponent<Rigidbody>().isKinematic = true;
-
-            slot1_key.transform.position = slot1.transform.position;
-            slot1.GetComponent<Collider>().enabled = false;
-
-            slot1_key.GetComponent<KeyCoreObject>().enabled = false;
-            slot1_key.tag = "Untagged";
-        }
-
-        if (slot2_key)
-        {
-            slot2_key.GetComponent<KeyCoreObject>().isActive = false;
-            slot2_key.GetComponent<KeyCoreObject>().isMoving = false;
-            slot2_key.GetComponent<Rigidbody>().isKinematic = true;
-
-            slot2_key.transform.position = slot2.transform.position;
-            slot2.GetComponent<Collider>().enabled = false;
-
-            slot2_key.GetComponent<KeyCoreObject>().enabled = false;
-            slot2_key.tag = "Untagged";
-        }
-    }*/
 }
