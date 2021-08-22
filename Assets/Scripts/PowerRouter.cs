b985954971d10dbb0f67cbde82e880d2453ee070
gav427 <102111726@student.swin.edu.au>
2021-08-22 18:18:43 +1000

    PowerRouter.cs: WIP

--- Assets/Scripts/PowerRouter.cs
+++ Assets/Scripts/PowerRouter.cs
@@ -9,13 +9,13 @@ public class PowerRouter : SlotPuzzle
     public GameObject inputSlot;
     public GameObject outputSlot;
 
-    private GameObject[,] grid;
+    private int[,] grid;
 
     private new void Start()
     {
         base.Start();
 
-        grid = new GameObject[gridSize.x,gridSize.y];
+        grid = new int[gridSize.x,gridSize.y];
     }
 
     private void Update()
@@ -24,8 +24,33 @@ public class PowerRouter : SlotPuzzle
         {
             for (int j=0;j<gridSize.y;j++)
             {
-                grid[i, j] = slot_parts[i + j];
+                if (slot_parts[i + j] && slot_parts[i + j].GetComponent<PowerBlockObject>())
+                {
+                    grid[i, j] = i + j;
+                }
             }
         }
     }
+
+    private void CheckConnection()
+    {
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            for (int j = 0; j < gridSize.y; j++)
+            {
+                if (grid[i,j] == slots.IndexOf(inputSlot))
+                {
+                    //WIP
+                }
+            }
+        }
+    }
+
+    /*private bool IsAdjacent(Vector2Int a, Vector2Int b)
+    {
+        if ((a.x - b.x) <= 1 && (a.y - b.y) <= 1)
+            return true;
+
+        return false;
+    }*/
 }
