d9641b4bf2d3443054a144e187fa2cf0f2234cfe
gav427 <102111726@student.swin.edu.au>
2021-10-02 21:06:10 +1000

    SlotPuzzle.cs: commenting out some disabling code to allow for mistakes without reseting the level

--- Assets/Scripts/SlotPuzzle.cs
+++ Assets/Scripts/SlotPuzzle.cs
@@ -22,18 +22,18 @@ public class SlotPuzzle : Puzzle
 
     protected void OnCollisionEnter(Collision collision)
     {
-        collision.GetContact(0).otherCollider.gameObject.GetComponent<SlotPartObject>().isActive = false;
-        collision.GetContact(0).otherCollider.gameObject.GetComponent<SlotPartObject>().isMoving = false;
-        collision.GetContact(0).otherCollider.attachedRigidbody.isKinematic = true;
+        //collision.GetContact(0).otherCollider.gameObject.GetComponent<SlotPartObject>().isActive = false;
+        //collision.GetContact(0).otherCollider.gameObject.GetComponent<SlotPartObject>().isMoving = false;
+        //collision.GetContact(0).otherCollider.attachedRigidbody.isKinematic = true;
 
         collision.GetContact(0).otherCollider.transform.position = collision.GetContact(0).thisCollider.transform.position;
         collision.GetContact(0).otherCollider.transform.rotation = collision.GetContact(0).thisCollider.transform.rotation;
 
         slot_parts[slots.IndexOf(collision.GetContact(0).thisCollider.gameObject)] = collision.GetContact(0).otherCollider.gameObject;
 
-        collision.GetContact(0).thisCollider.enabled = false;
+        //collision.GetContact(0).thisCollider.enabled = false;
 
-        collision.GetContact(0).otherCollider.gameObject.GetComponent<SlotPartObject>().enabled = false;
-        collision.GetContact(0).otherCollider.gameObject.tag = "Untagged";
+        //collision.GetContact(0).otherCollider.gameObject.GetComponent<SlotPartObject>().enabled = false;
+        //collision.GetContact(0).otherCollider.gameObject.tag = "Untagged";
     }
 }
