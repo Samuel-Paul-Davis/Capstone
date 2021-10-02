3cbeecee72551f5b9ed82848cb6bd2fc6c6a5f37
gav427 <102111726@student.swin.edu.au>
2021-10-03 00:03:46 +1000

    Top Level.unity: improving power routing puzzle + PowerBlockObject.cs: adding material change when powered on + MovableObject.cs: changing access modifiers for benefit of child class PowerBlockObject

--- Assets/Scripts/MovableObject.cs
+++ Assets/Scripts/MovableObject.cs
@@ -10,13 +10,13 @@ public class MovableObject : AbstractObjectInteraction
     public bool isActive;
 
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    protected void Update()
     {
         if (isActive)
         {
--- Assets/Scripts/PowerBlockObject.cs
+++ Assets/Scripts/PowerBlockObject.cs
@@ -5,9 +5,32 @@ using UnityEngine;
 public class PowerBlockObject : SlotPartObject
 {
     public bool isPowered = false;
+    public Material[] activatedMaterials;
+
+    private Material[] originalMaterials;
 
     /*
      * Add Discover() override?
      *      - could show power flow with correct materials / shaders
      */
+
+    private new void Start()
+    {
+        base.Start();
+
+        originalMaterials = gameObject.GetComponent<MeshRenderer>().materials;
+    }
+
+    private new void Update()
+    {
+        base.Update();
+
+        if (isPowered)
+        {
+            gameObject.GetComponent<MeshRenderer>().materials = activatedMaterials;
+        } else
+        {
+            gameObject.GetComponent<MeshRenderer>().materials = originalMaterials;
+        }
+    }
 }
