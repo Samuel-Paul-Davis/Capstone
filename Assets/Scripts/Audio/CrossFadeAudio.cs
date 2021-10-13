84fe2b326c5bdbe6deb5b5452ef44095d1903f6c
Giovanni <gianni35011@gmail.com>
2021-10-13 23:11:12 +1100

    Crossfading music

---
+++ Assets/Scripts/Audio/CrossFadeAudio.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossFadeAudio : MonoBehaviour
+{
+    public string audioGroupParam;
+    public bool onTrigger;
+    public bool onStart;
+
+    //private void Start()
+    //{
+    //    if (onStart)
+    //    {
+    //        SingletonMonoBehaviour<MusicManager>.Instance.CrossFadeMusic(audioGroupParam);
+    //    }
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (onTrigger)
+    //    {
+    //        SingletonMonoBehaviour<MusicManager>.Instance.CrossFadeMusic(audioGroupParam);
+    //    }    
+    //}
+
+}
