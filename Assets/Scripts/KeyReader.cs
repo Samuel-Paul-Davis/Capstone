7eea022488cc9c979d6dfa44168c455a7a01e600
gav427 <102111726@student.swin.edu.au>
2021-10-16 20:28:53 +1100

    Keycard Puzzle: fixing compatability with final puzzle

--- Assets/Scripts/KeyReader.cs
+++ Assets/Scripts/KeyReader.cs
@@ -14,6 +14,14 @@ public class KeyReader : SlotPuzzle
     {
         for (int i=0; i<slots.Count;i++)
         {
+            if (slots[i].transform.childCount > 0 && slots[i].transform.GetChild(0).GetComponent<SlotPartObject>() != null)
+                unlocked = true;
+            else
+                unlocked = false;
+        }
+
+        /*for (int i=0; i<slots.Count;i++)
+        {
             if (slot_parts[i] && slot_parts[i].GetComponent<KeyCoreObject>())
                 unlocked = true;
             else
@@ -21,6 +29,6 @@ public class KeyReader : SlotPuzzle
                 unlocked = false;
                 break;
             }
-        }
+        }*/
     }
 }
--- Assets/Scripts/SlotPuzzle.cs
+++ Assets/Scripts/SlotPuzzle.cs
@@ -7,7 +7,7 @@ public class SlotPuzzle : Puzzle
     public List<GameObject> slots;
 
     [SerializeField]
-    protected GameObject[] slot_parts;
+    //protected GameObject[] slot_parts;
 
     protected void Start()
     {
@@ -17,6 +17,30 @@ public class SlotPuzzle : Puzzle
             if (child != transform)
                 slots.Add(child.gameObject);
 
-        slot_parts = new GameObject[slots.Count];
+        //slot_parts = new GameObject[slots.Count];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<SlotPartObject>() != null)
+        {
+            bool sucess = false;
+            for (int i=0;i<transform.childCount;i++)
+            {
+                if (transform.GetChild(i).gameObject.activeInHierarchy && transform.GetChild(i).childCount == 0) {
+                    other.transform.SetParent(transform.GetChild(i), false);
+                    sucess = true;
+                }
+
+                if (!sucess)
+                    return;
+            }
+
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.transform.localPosition = Vector3.zero;
+            other.transform.localRotation = Quaternion.identity;
+
+            //slot_parts[slots.IndexOf(gameObject)] = other.gameObject;
+        }
     }
 }
