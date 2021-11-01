dbb6f1b992de181bf30862d92bfb4cead2dfebe0
pshijingg <80439561+pshijingg@users.noreply.github.com>
2021-11-01 19:09:33 +1100

    Edited dialog

---
+++ Assets/DialogueEditor/Assets/Scripts/MyScripts/TopLevelDialog2_Trigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+
+public class TopLevelDialog2_Trigger : MonoBehaviour
+{
+
+    public NPCConversation myConversation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ConversationManager.Instance.StartConversation(myConversation);
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
