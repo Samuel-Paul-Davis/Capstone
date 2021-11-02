e41653f184e44c4b05dae6469334f668651ae496
pshijingg <80439561+pshijingg@users.noreply.github.com>
2021-11-03 03:37:39 +1100

    added dialog to restored desert

---
+++ Assets/DialogueEditor/Assets/Scripts/MyScripts/RestoredReturnDialogTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class RestoredReturnDialogTrigger : MonoBehaviour
+{
+
+    public NPCConversation myConversation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ConversationManager.Instance.StartConversation(myConversation);
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+}
